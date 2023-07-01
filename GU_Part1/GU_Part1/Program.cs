0f1aa59683ecc1327ecaa924b0cd7d8505f75f6d
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-01 21:06:05 +0700

    BT lời chào

---
+++ GU_Part1/GU_Part1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test //
+{
+    class Buoi1_test
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your full name : ");
+            string input_name = Console.ReadLine();
+            Console.WriteLine("Hello and welcome, " + input_name +"!");
+        }
+    }
+
+}
