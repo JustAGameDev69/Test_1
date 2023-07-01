eff07602ba9024263d207b24d89032c00377a66d
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-01 21:26:09 +0700

    BT quy đổi

---
+++ GU_Part1/MoneyExchange_BasicC#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Money_Exchange
+{
+    static void Main(string[] args)
+    {
+        // KHAI BÁO CÁC BIẾN:
+        int input_usd, output_vnd, final_result;
+        int exchange_value = 23000;
+
+        //QUY ĐỔI:
+        Console.Write(" USD : ");
+        string user_input = Console.ReadLine();
+        final_result = int.Parse(user_input) * exchange_value;
+        Console.WriteLine(" VND : " + final_result + " VND" );
+
+    }
+}
