eb9227443472d54a2ef6ff5489785b524f5360a3
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-03 07:40:31 +0700

    Homework

---
+++ GU_Part1/System_Time/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace System_Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time_now = DateTime.Now;
+            Console.WriteLine("Now is " + time_now);
+        }
+    }
+}
