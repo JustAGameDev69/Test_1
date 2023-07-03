dd0ffc5eab46c29a7ad649aa49697dd9f229e3db
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-03 07:53:41 +0700

    Homework

---
+++ GU_Part1/RectangleArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Rectangle_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width, height, area_result;
+
+            Console.Write("Input rectangle width: ");
+            width = float.Parse(Console.ReadLine());
+
+            Console.Write("Input rectangle height: ");
+            height = float.Parse(Console.ReadLine());
+
+            area_result = width * height;
+
+            Console.WriteLine("Area of this rectangle is: " + area_result);
+        }
+    }
+}
