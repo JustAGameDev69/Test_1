a4199957769298f7c57e264bdf20e120235da272
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-06 08:35:58 +0700

    main

---
+++ C#Basic_Part2/C#Basic_Part2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is a simple linear equation solver!");
+
+            Console.Write("Please input a in the equation ( ax + b = 0 ) here: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please input b in the equation ( ax + b = 0 ) here: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a==0)
+            {
+                if(b==0)
+                {
+                    Console.WriteLine("The solution is all x!");
+                }
+                else
+                {
+                    Console.WriteLine("There's no solution!");
+                }
+
+            }
+            else
+            {
+                double result = -b / a;
+                Console.WriteLine("The solution is: " + result);
+            }
+
+
+        }
+    }
+}
