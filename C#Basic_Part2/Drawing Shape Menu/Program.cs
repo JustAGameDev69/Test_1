55319f35e2867a58bdfbbd1bfa4b8fd665523c29
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-07 22:16:47 +0700

    main

---
+++ C#Basic_Part2/Drawing Shape Menu/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int choice;
+
+        while (true)
+        {
+
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Draw the rectangle");
+            Console.WriteLine("2. Draw the square triangle");
+            Console.WriteLine("3. Draw the rectangle");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("Enter your choice: ");
+            choice = Int32.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("This is a retangle!");
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        for (int j = 1; j <= 7; j++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("This is a square bottom-left triangle!");
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        for (int j = 1; j <= i; j++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("This is a square bottom-right triangle!");
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        for (int j = 1; j <= 5; j++)
+                        {
+                            if (j >= 5 - i + 1)
+                            {
+                                Console.Write("*");
+                            }
+                            else
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("This is a square top-left triangle!");
+                    for (int i = 1; i <= 6; i++)
+                    {
+                        for (int j = 1; j <= 6; j++)
+                        {
+                            if (j >= 6 - i + 1)
+                            {
+                                Console.Write(" ");
+                            }
+                            else
+                            {
+                                Console.Write("*");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("This is a square top-right triangle!");
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        for (int j = 5; j >= 1; j--)
+                        {
+                            if (j <= 5 - i + 1)
+                            {
+                                Console.Write("*");
+                            }
+                            else
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 3:
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        for (int j = 1; j <= 5 - i; j++)
+                        {
+                            Console.Write(" ");
+                        }
+                        for (int k = 1; k <= 2 * i - 1; k++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("No choice!");
+                    break;
+
+            }
+        }
+    }
+}
