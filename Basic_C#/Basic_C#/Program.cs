2be8252d5bfc7d1b66a7968900b56f2dcfde56e1
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-09 09:45:28 +0700

    main

---
+++ Basic_C#/Basic_C#/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Array_Total
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter how many numbers: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            int[] arr = new int[n];
+
+            for(int i =0; i < n; i++)
+            {
+                Console.Write("Enter the {0} number: ", i+1);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int elements_total = 0;
+            foreach (int item in arr)
+            {
+                elements_total += item;
+            }
+            Console.WriteLine("Total elements in this array: " + elements_total);
+
+        }
+    }
+}
---
+++ C#_Basic4/C#_Basic4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Schema;
+
+namespace BasicCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter size of array: ");
+            n = int.Parse(Console.ReadLine());
+            int total_ele = 0;
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter the {0} numbers in this array: ", i+1);
+                int user_input = int.Parse(Console.ReadLine());
+                arr[i] = user_input;
+            }
+
+            foreach (int item in arr)
+            {
+                total_ele += item;
+            }
+            Console.WriteLine("Total of elements in this array is: " + total_ele);
+        }
+    }
+}
