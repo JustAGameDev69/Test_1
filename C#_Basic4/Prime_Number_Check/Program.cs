f7d70b5ac0321421bc69a5123cce15658a6c2957
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-11 09:40:30 +0700

    main

---
+++ C#_Basic4/Prime_Number_Check/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Prime_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool is_prime = false;
+            int n;
+
+            Console.Write("This is prime number checker! Enter a numbers: ");
+            n = int.Parse(Console.ReadLine());
+
+            if(n < 2)
+            {
+                is_prime = false;
+            }
+            else
+            {
+                int i = 2;
+                is_prime = true;
+                while (i<=Math.Sqrt(n))
+                {
+                    if(i%n ==0)
+                    {
+                        is_prime = false;
+                        break;
+                    }
+                    i++;
+                }
+
+                if(is_prime)
+                {
+                    Console.WriteLine(n + "is a prime number!");
+                }
+                else
+                {
+                    Console.WriteLine(n + "isn't a prime number!");
+                }
+            }
+        }
+    }
+}
