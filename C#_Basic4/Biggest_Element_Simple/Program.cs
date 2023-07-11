e9979eaf8b40bbcc8ade5e3f8112522058d2ba26
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-11 09:58:43 +0700

    main

---
+++ C#_Basic4/Biggest_Element_Simple/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace intermiami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size: ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            int[] arr = new int[n];
+
+
+            Console.WriteLine("Enter array elements!");
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("arr[{0}] : ", i);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            int max_element = arr[0];
+            for (int i = 0;i < arr.Length;i++)
+            {
+                if (max_element < arr[i])
+                {
+                    max_element = arr[i];
+                }
+            }
+
+            Console.WriteLine("The biggest element in this array is " + max_element);
+        }
+    }
+}
